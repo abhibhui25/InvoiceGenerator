47f57b2d5140f456aedd353c3e8da8c1bbf0a972
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-10-26 20:25:29 +0530

    added a method to calculate fare for multiple rides and test the same

--- main/java/com/cg/invoicegenerator/InvoiceGenerator.java
+++ main/java/com/cg/invoicegenerator/InvoiceGenerator.java
@@ -13,4 +13,12 @@ public class InvoiceGenerator {
         else
             return totalFare;
     }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare=0;
+        for (Ride ride:rides){
+            totalFare += calculateFare(ride.distance,ride.time);
+        }
+        return totalFare;
+    }
 }
--- test/java/com/cg/invoicegenerator/InvoiceGeneratorTest.java
+++ test/java/com/cg/invoicegenerator/InvoiceGeneratorTest.java
@@ -20,4 +20,14 @@ public class InvoiceGeneratorTest {
         double fare=InvoiceGenerator.calculateFare(distance,time);
         Assert.assertEquals(5,fare,0.0);
     }
+    @Test
+    public void GivenMultipleRides_ShouldReturnTotalFare(){
+        InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
+        Ride rides[] ={new Ride(3.0,5),
+                      new Ride(0.2,1)
+                      };
+        double fare=invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(40,fare,0.0);
+
+    }
 }
